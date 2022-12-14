95278ab2892d5a151ffa70f1d05d062a4758f24b
rpalival <112983661+rpalival@users.noreply.github.com>
2022-12-13 19:03:53 -0500

    Initial commit

---
+++ MRTD.py
@@ -0,0 +1,139 @@
+"""
+@author - RAJ PALIVAL
+This python file has 2 Dummy functions namely HardwareScanner() and sampleDatabase(),
+These dummy functions will be mocked in the unittest file.
+
+The other main functions are: decode() and encode()
+decode: takes in a complete MRZ string from hardware scanner and returns all the information fields in their respective variables
+encode: takes in a complete information of the passport holder as a dictionary and return the complete MRZ strip.
+        along with the certain information fields whose check digit is to be computed
+
+There are two seperate functions to implement the check-digit calculation:
+charValue(): This function takes in a character and returns the numeric value of that character starting from A=10 B=11...and '<' = 0
+checkDigit(): This funtion takes in a information field from the encode function in form of a string and returns the check digit for that field
+
+Last function which is mismatch:
+mismatch(): This function compares the checkdigit from decode() and checkdigit computed from checkDigit() and
+            matches them, if there is a mismatch it outputs which information field did the mismatch happen.
+
+"""
+class HardwareScanner():   #Dummy hardware Scanner function 1; giving out two random MRZ data to be used by function 2
+    # MRZ = "P<LKABLACK<<DOMINIQUE<CORALIE<<<<<<<<<<<<<<<;R724644M28LKA6301307M6801184RZ671583Y<<<<<<6"
+    pass
+
+def decode():
+    MRZLine = HardwareScanner()
+    sName = MRZLine.MRZ
+    lines = sName.split(";")
+    L1 = lines[0]
+    L2 = lines[1]
+    Lfirstsplit = L1.split("<<")[0]
+    Rfirstsplit = L1.split("<<")[1]
+    document_type = Lfirstsplit.split("<")[0]
+    issuing_country = Lfirstsplit.split("<")[1][:3]
+    last_name = Lfirstsplit.split("<")[1][3:]
+    if('<' in Rfirstsplit):
+        given_name = Rfirstsplit.split("<")
+        given_name = str(given_name[0]+ " " +given_name[1])
+    else:
+        given_name = Rfirstsplit
+
+    passport_number = L2[:9]
+    passport_number_Cdigit = L2[9]
+    country_code = L2[10:13]
+    birth_date = L2[13:19]
+    birth_date_Cdigit = L2[19]
+    sex = L2[20]
+    expiration_date = L2[21:27]
+    expiration_date_Cdigit = L2[27]
+    personal_number = L2[28:37]
+    personal_number_Cdigit = L2[-1]
+
+    return (passport_number_Cdigit, birth_date_Cdigit, expiration_date_Cdigit, personal_number_Cdigit,
+    passport_number, birth_date, expiration_date,personal_number,country_code, last_name, given_name,
+    document_type, issuing_country,sex)
+
+# ('4', '6', '2', '6', 'W620126G5', '591010', '970730', 'AJ010215I', 'CIV', 'LYNN', 'NEVEAH BRAM', 'P', 'CIV', 'F')
+# print(decode())
+class sampleDatabase():
+    # sampleData = {
+    #     "line1": {
+    #         "issuing_country": "LKA",
+    #         "last_name": "BLACK",
+    #         "given_name": "DOMINIQUE CORALIE"
+    #     },
+    #     "line2": {
+    #         "passport_number": "R724644M2",
+    #         "country_code": "LKA",
+    #         "birth_date": "630130",
+    #         "sex": "M",
+    #         "expiration_date": "680118",
+    #         "personal_number": "RZ671583Y"
+    #     }
+    # }
+    pass
+
+# sampledict = sampleDatabase()
+# print(decode(HardwareScanner()))
+
+def charValue(c):
+    if c >= '0' and c <= '9':
+        return ord(c) - ord('0')
+    if c >= 'A' and c <= 'Z':
+        return ord(c) - ord('A') + 10
+    if c == '<':
+        return 0
+
+def checkDigit(data):
+    multiplier = [7, 3, 1]
+    sum = 0
+    for index,char in enumerate(data):
+        product = charValue(char) * multiplier[index % len(multiplier)]
+        sum += product
+    cdigit = sum % 10
+    return cdigit
+
+
+# checkDigit(data)
+# print(cdigit)
+#L898902C3
+
+def encode():
+    dictObj = sampleDatabase()
+    d = dictObj.sampleData
+    given_name = d['line1']['given_name']
+    if(" " in (d['line1']['given_name'])):
+        given_name = given_name.replace(" ","<")
+    else:
+        given_name = given_name
+    passport_number = d['line2']['passport_number']
+    birth_date = d['line2']['birth_date']
+    expiration_date = d['line2']['expiration_date']
+    personal_number = d['line2']['personal_number']
+    line1 = f"P<{d['line1']['issuing_country']+d['line1']['last_name']}<<{given_name}"
+    N = 44 - len(line1)
+    finalline1 = str(line1.ljust(N + len(line1),'<'))
+
+    line2 = f"{passport_number+str(checkDigit(passport_number))}{d['line2']['country_code']}"
+    line2a = f"{birth_date+str(checkDigit(birth_date))+d['line2']['sex']}"
+    line2b = f"{expiration_date+str(checkDigit(expiration_date))+personal_number}"
+    finalline2 = str(line2+line2a+line2b)+f"<<<<<<{str(checkDigit(personal_number))}"
+    finalline = finalline1+";"+finalline2
+    return passport_number,birth_date,expiration_date,personal_number,finalline
+# print(encode())
+
+var_dict = {0:'Passport Number', 1:'Birth Date', 2:'Expiration Date', 3:'Personal Number'}
+def mismatch():
+    a = True
+    for x in range(len(var_dict)):
+        digit= decode()[x]
+        field = checkDigit(encode()[x])
+
+        if(str(digit) != str(field)):
+            a = False
+            value = (f"mismatch found in {var_dict[x]}")
+    if(a==True):
+        value = ("No mismatch found!")
+    return value
+        
+# mismatch(decode()[0],checkDigit(encode()[0]))
---
+++ MTTDtest.py
@@ -0,0 +1,99 @@
+import unittest
+from unittest.mock import patch
+from MRTD import decode,encode,mismatch,checkDigit
+
+#Mock Classes for Hardware Scanner which has sample data in form of a string
+class MockResponse():
+    MRZ = "P<CIVLYNN<<NEVEAH<BRAM<<<<<<<<<<<<<<<<<<<<<<;W620126G54CIV5910106F9707302AJ010215I<<<<<<6",
+    sampleData = {
+            "line1": {
+                "issuing_country": "CIV",
+                "last_name": "LYNN",
+                "given_name": "NEVEAH BRAM"
+            },
+            "line2": {
+                "passport_number": "W620126G5",
+                "country_code": "CIV",
+                "birth_date": "591010",
+                "sex": "F",
+                "expiration_date": "970730",
+                "personal_number": "AJ010215I"
+            }
+        }
+class MockResponse1():
+    MRZ = "P<ABWMALDONADO<<CAMILLA<<<<<<<<<<<<<<<<<<<<<;V008493B64ABW7809095M0909088QZ181922T<<<<<<5",
+    sampleData = {
+            "line1": {
+                "issuing_country": "ABW",
+                "last_name": "MALDONADO",
+                "given_name": "CAMILLA"
+            },
+            "line2": {
+                "passport_number": "V008493B6",
+                "country_code": "ABW",
+                "birth_date": "780909",
+                "sex": "M",
+                "expiration_date": "090908",
+                "personal_number": "QZ181922T"
+            }
+        }
+class MockResponse2():
+    MRZ = "P<LKABLACK<<DOMINIQUE<CORALIE<<<<<<<<<<<<<<<;R724644M28LKA6301307M6801184RZ671583Y<<<<<<6",
+    sampleData = {
+            "line1": {
+                "issuing_country": "LKA",
+                "last_name": "BLACK",
+                "given_name": "DOMINIQUE CORALIE"
+            },
+            "line2": {
+                "passport_number": "R724644M2",
+                "country_code": "LKA",
+                "birth_date": "630130",
+                "sex": "M",
+                "expiration_date": "680118",
+                "personal_number": "RZ671583Y"
+            }
+        }
+
+
+
+class TestMRTD(unittest.TestCase):
+    #Test case 1: to mock hardware scanner and to test decode function
+    @patch('MRTD.HardwareScanner', side_effect = MockResponse)
+    def test_decode(self, mock_obj):
+        self.assertEqual(decode(),('4', '6', '2', '6', 'W620126G5', '591010', '970730', 'AJ010215I', 'CIV', 'LYNN', 'NEVEAH BRAM', 'P', 'CIV', 'F'))
+    
+    #Test case 2: to mock hardware scanner and to test decode function on a person with single given name
+    @patch('MRTD.HardwareScanner', side_effect = MockResponse1)
+    def test_decode1(self, mock_obj1):
+        self.assertEqual(decode(),('4', '5', '8', '5', 'V008493B6', '780909', '090908', 'QZ181922T', 'ABW', 'MALDONADO', 'CAMILLA', 'P', 'ABW', 'M'))
+    
+    #Test case 3: to mock hardware scanner and to test decode function on a person with first name and last name as given name
+    @patch('MRTD.HardwareScanner', side_effect = MockResponse2)
+    def test_decode2(self, mock_obj2):
+        self.assertEqual(decode(),('8', '7', '4', '6', 'R724644M2', '630130', '680118', 'RZ671583Y', 'LKA', 'BLACK', 'DOMINIQUE CORALIE', 'P', 'LKA', 'M'))
+
+    #Test case 4: to mock Database and to test encode function
+    @patch('MRTD.sampleDatabase', side_effect = MockResponse)
+    def test_encode(self, mock_obj3):
+        self.assertEqual(encode(),('W620126G5', '591010', '970730', 'AJ010215I', 'P<CIVLYNN<<NEVEAH<BRAM<<<<<<<<<<<<<<<<<<<<<<;W620126G54CIV5910106F9707302AJ010215I<<<<<<6'))
+
+    #Test case 5: to mock Database and to test encode function for the same person as in my 2nd test case
+    @patch('MRTD.sampleDatabase', side_effect = MockResponse1)
+    def test_encode1(self, mock_obj4):
+        self.assertEqual(encode(),('V008493B6', '780909', '090908', 'QZ181922T', 'P<ABWMALDONADO<<CAMILLA<<<<<<<<<<<<<<<<<<<<<;V008493B64ABW7809095M0909088QZ181922T<<<<<<5'))
+
+    #Test case 6: to mock Database and to test encode function for the same person as in my 3rd test case
+    @patch('MRTD.sampleDatabase', side_effect = MockResponse2)
+    def test_encode2(self, mock_obj5):
+        self.assertEqual(encode(),('R724644M2', '630130', '680118', 'RZ671583Y', 'P<LKABLACK<<DOMINIQUE<CORALIE<<<<<<<<<<<<<<<;R724644M28LKA6301307M6801184RZ671583Y<<<<<<6'))
+
+    #Test case 7: to test mismatch function by checking the hardware scanner check digit with the computed check digit from database
+    # def test_mismatch(self):
+    #     self.assertEqual(mismatch(),"No mismatch found!")
+        
+    
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
